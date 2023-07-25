e2c5f7d7a0d3711186f7125512a3c04eb46e6eab
rangggu <aliyah521@naver.com>
2023-07-25 14:42:22 +0900

    Fix : 파일명이 변경되지 않은 오류 수정

---
+++ src/components/common/_Common.interface.ts
@@ -0,0 +1,12 @@
+export interface CommonWrapperProps {
+  children: React.ReactNode;
+}
+
+export interface LoggerProps {
+  start: string | undefined;
+  end: string | undefined;
+}
+
+export interface SkeletonComponentProps {
+  type: string;
+}

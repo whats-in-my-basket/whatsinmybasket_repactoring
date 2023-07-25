fa1958f63f1b2dfc3a2ed54086499b126a6eac33
rangggu <aliyah521@naver.com>
2023-07-25 14:29:15 +0900

    Feat : 장바구니 수량 표시 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./styles/main.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Main from "./pages/Main";
+import Category from "./pages/Category";
+import Detail from "./pages/Detail";
+import Cart from "./pages/Cart";
 import Footer from "./components/footer/Footer";
 
 export default function App() {
@@ -10,6 +13,9 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/:category" element={<Category />}></Route>
+        <Route path="/detail/:id" element={<Detail />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/header/HeaderBasketBtn.tsx
+++ src/components/header/HeaderBasketBtn.tsx
@@ -1,10 +1,15 @@
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../store/configureStore";
 
 export default function HeaderBasketBtn() {
+  const cartItem = useAppSelector((state: any) => state.cart);
+  const count = cartItem.length;
+
   return (
     <Link to="/cart" className="header-basket-btn">
       <HiOutlineShoppingBag className="header-basket-btn-img" />
+      <span className="count">{count}</span>
     </Link>
   );
 }

c203963f174beb2433f05e6b26a78f99529e24f6
rangggu <aliyah521@naver.com>
2023-07-25 14:24:59 +0900

    Feat : LazyLoad 및 스켈레톤 UI 적용

--- src/components/product/Card.tsx
+++ src/components/product/Card.tsx
@@ -1,15 +1,35 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { ProductProps } from "./_Card.interface";
+import LazyLoad from "react-lazy-load";
+import { CardProps } from "./_Card.interface";
+import SkeletonComponent from "../common/skeleton/SkeletonComponent";
 
-export default function Card({ title, image, price, id }: ProductProps) {
+export default function Card({ title, image, price, id, category }: CardProps) {
+  const [isTitle, setIsTitle] = useState<boolean>(false);
+  const [isImage, setIsImage] = useState<boolean>(false);
+  const [isPrice, setIsPrice] = useState<boolean>(false);
+
+  useEffect(() => {
+    title && setIsTitle(true);
+    image && setIsImage(true);
+    price && setIsPrice(true);
+  }, [title, image, price, id]);
+
   return (
-    <Link className="card" to={`/product/${id}`}>
-      <figure>
-        <img className="card-image" src={`${image}`}></img>
+    <Link className="card" to={`/detail/${id}`} state={{ category: category }}>
+      <figure className="card-image-area">
+        {isImage && (
+          <LazyLoad className="card-lazy-load-area">
+            <img className="card-image" src={image} loading="lazy"></img>
+          </LazyLoad>
+        )}
+        {!isImage && <SkeletonComponent type="image" />}
       </figure>
       <div className="card-info-area">
-        <p className="card-title">{title}</p>
-        <p className="card-price">{`$${Math.round(price)}`}</p>
+        {isTitle && <p className="card-title">{title}</p>}
+        {!isTitle && <SkeletonComponent type="title" />}
+        {isPrice && <p className="card-price">{`$${Math.round(price)}`}</p>}
+        {!isPrice && <SkeletonComponent type="price" />}
       </div>
     </Link>
   );

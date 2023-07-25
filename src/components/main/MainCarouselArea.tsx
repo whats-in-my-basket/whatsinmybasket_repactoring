0b6ce473959930e267d7859f781b897a6f15b84a
rangggu <aliyah521@naver.com>
2023-07-25 15:45:48 +0900

    Fix : 배포시 이미지 경로 오류 수정

--- src/components/main/MainCarouselArea.tsx
+++ src/components/main/MainCarouselArea.tsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 import MainCarouselItem from "./MainCarouselItem";
+import fashionImage from "../../src/assets/image/fashion-image.jpg";
+import fashionImageWebp from "../../src/assets/image/fashion-image.webp";
+import accessoryImage from "../../src/assets/image/accessory-image.jpg";
+import accessoryImageWebp from "../../src/assets/image/accessory-image.webp";
+import digitalImage from "../../src/assets/image/digital-image.jpg";
+import digitalImageWebp from "../../src/assets/image/digital-image.webp";
 
 export default function MainCarouselArea() {
   const [index, setIndex] = useState(0);
@@ -12,24 +18,24 @@ export default function MainCarouselArea() {
       desc: "이제 막 도착한 패션 청바지를 구경해보세요.",
       id: 1,
       url: "/fashion",
-      image: "../../src/assets/image/fashion-image.jpg",
-      imageWebp: "../../src/assets/image/fashion-image.webp",
+      image: fashionImage,
+      imageWebp: fashionImageWebp,
     },
     {
       title: "신속한 업무처리!",
       desc: "다양한 디지털 상품을 둘러보세요.",
       id: 2,
       url: "/digital",
-      image: "../../src/assets/image/digital-image.jpg",
-      imageWebp: "../../src/assets/image/digital-image.webp",
+      image: digitalImage,
+      imageWebp: digitalImageWebp,
     },
     {
       title: "신선한 식품",
       desc: "농장 직배송으로 더욱 신선한 식료품을 만나보세요.",
       id: 3,
       url: "/accessory",
-      image: "../../src/assets/image/accessory-image.jpg",
-      imageWebp: "../../src/assets/image/accessory-image.webp",
+      image: accessoryImage,
+      imageWebp: accessoryImageWebp,
     },
   ];
 

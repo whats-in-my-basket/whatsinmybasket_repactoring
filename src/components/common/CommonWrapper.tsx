308b5d349921e5ecf1ed9d5d2505827bacfc03c0
rangggu <aliyah521@naver.com>
2023-07-25 14:18:18 +0900

    Refactor : 공통 wrapper 컴포넌트 파일 위치 변경

--- src/components/common/CommonWrapper.tsx
+++ src/components/common/CommonWrapper.tsx
@@ -1,4 +1,4 @@
-import { CommonWrapperProps } from "./_common.interface";
+import { CommonWrapperProps } from "./_Common.interface";
 
 export default function CommonWrapper({ children }: CommonWrapperProps) {
   return <div className="common-wrapper">{children}</div>;

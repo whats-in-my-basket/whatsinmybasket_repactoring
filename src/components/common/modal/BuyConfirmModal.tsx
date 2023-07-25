fc863398a03aa403d2505095caa2c88f909daf6a
rangggu <aliyah521@naver.com>
2023-07-25 14:21:52 +0900

    Feat : 구매 확인 모달 구현

---
+++ src/components/common/modal/BuyConfirmModal.tsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+import { useAppDispatch } from "../../../store/configureStore";
+import { setModalClose } from "../../../store/modules/cartModal";
+import { HiOutlineX } from "react-icons/hi";
+import { useNavigate } from "react-router";
+
+export default function BuyConfirmModal() {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const modalClose = () => {
+    dispatch(setModalClose());
+  };
+
+  const modalOutSideClick = (e: any) => {
+    if (modalRef.current === e.target) {
+      modalClose();
+    }
+  };
+
+  const goMain = () => {
+    modalClose();
+    navigate("/");
+  };
+
+  return (
+    <div className="modal-background" ref={modalRef} onClick={(e) => modalOutSideClick(e)}>
+      <div className="modal-container">
+        <div className="modal-wrapper">
+          <button className="modal-close-btn" onClick={modalClose}>
+            <HiOutlineX className="modal-close-btn-icon" />
+          </button>
+          <div className="modal-contents">
+            <h2 className="modal-contents-title">정말로 구매하시겠습니까?</h2>
+            <p className="modal-contents-desc">장바구니의 모든 상품들이 삭제됩니다.</p>
+          </div>
+          <button className="modal-ok-btn" onClick={goMain}>
+            구매하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
